8a7a7836543e2aa9f2d50c1fd9eb18d6a221550f
littlezim <littlezim18@gmail.com>
2016-10-13 16:05:43 -0700

    Additional Cards Added to DB

--- Bunchkins/Migrations/Configuration.cs
+++ Bunchkins/Migrations/Configuration.cs
@@ -21,7 +21,7 @@ namespace Bunchkins.Migrations
         {
             if(context.Cards.Count() == 0)
             {
-                
+                //Door Cards
                 context.Cards.Add(new BuffMonsterSpellDoorCard { Description = "Play During Combat. +5 to Monster Level.", FlavorText = "You'll like him when he's angry. If you aren't the one fighting.", Name = "Enraged Monster", CombatPower = 5, TreasureChange = 1, Type = "CombatSpell" });
                 context.Cards.Add(new BuffMonsterSpellDoorCard { Description = "Play During Combat. +5 to Monster Level.", FlavorText = "The square root of pie is, IT'S EATING ME!!!!", Name = "Intelligent Monster", CombatPower = 5, TreasureChange = 1, Type = "CombatSpell" });
                 context.Cards.Add(new BuffMonsterSpellDoorCard { Description = "Play During Combat. +10 to Monster Level.", FlavorText = "Her fingers will blot out the sun.", Name = "Humongous Monster", CombatPower = 5, TreasureChange = 2, Type = "CombatSpell" });
@@ -29,6 +29,7 @@ namespace Bunchkins.Migrations
                 context.Cards.Add(new BuffMonsterSpellDoorCard { Description = "Play During Combat. -5 to Monster Level.", FlavorText = "TOO.....CUTE....Gonna....DIE.", Name = "Baby Monster", CombatPower = -5, TreasureChange = -1, Type = "CombatSpell" });
                 
                 context.Cards.Add(new AddMonsterSpellCard { Description = "Add a random monster to this Combat Phase.", FlavorText = "This room looks fun. OH GOD WHAT'S THAT?!", Name = "Wandering Monster", Type = "CombatSpell" });
+                context.Cards.Add(new AddMonsterSpellCard { Description = "Add a random monster to this Combat Phase.", FlavorText = "A NEW CHALLENGER APPEARS!", Name = "Arcade Fight", Type = "CombatSpell" });
 
                 context.Cards.Add(new ArmorRemoveMonsterCard { Description = "Bad Stuff: Lose Headgear", FlavorText = "The biggest and most hairy of feet.", Name = "Bigfoot", Level = 12, TreasureGain = 3, LevelGain = 1, Slot = "Headgear", Type = "Monster" });
                 context.Cards.Add(new ArmorRemoveMonsterCard { Description = "Bad Stuff: Lose Footgear", FlavorText = "I wont ask how you got them. But you do have them.", Name = "Crabs", Level = 1, TreasureGain = 1, LevelGain = 1, Slot = "Footgear", Type = "Monster" });
@@ -79,13 +80,17 @@ namespace Bunchkins.Migrations
                 context.Cards.Add(new LoseEquipCurseCard { Description = "Target Loses 2 Hander", FlavorText = "It REALLY wanted your two hander.", Name = "Very Accurate Black Hole", Slot = "2Hands", Type = "Curse" });
 
                 context.Cards.Add(new LoseHandCardsCurseCard { Description = "Target Loses 2 Cards from Hand", FlavorText = "On the bright side, you can use toast as papertowels.", Name = "Butter Fingers", NumCards = 2 , Type = "Curse" });
+                context.Cards.Add(new LoseHandCardsCurseCard { Description = "Target Loses 1 Card from Hand", FlavorText = "An Underhanded tactic....but effective.", Name = "Poked In The Eye", NumCards = 1, Type = "Curse" });
+                context.Cards.Add(new LoseHandCardsCurseCard { Description = "Target Loses 3 Cards from Hand", FlavorText = "Just add water and wait 3 minutes.", Name = "Noodle Brain", NumCards = 3, Type = "Curse" });
+                context.Cards.Add(new LoseHandCardsCurseCard { Description = "Target Loses 1 Card from Hand", FlavorText = "Is that Big Foot?", Name = "Look Over There", NumCards = 1, Type = "Curse" });
 
                 context.Cards.Add(new LoseLevelCurseCard { Description = "Target Player Loses 2 Levels", FlavorText = "And here you thought it was a cute duck.", Name = "Duck of Doom", Levels = 2, Type = "Curse" });
                 context.Cards.Add(new LoseLevelCurseCard { Description = "Target Player Loses 1 Level", FlavorText = "You used to be awsome, but you can't remember how anymore.", Name = "Amnesia", Levels = 1, Type = "Curse" });
+                context.Cards.Add(new LoseLevelCurseCard { Description = "Target Player Loses 1 Level", FlavorText = "Don't even try to blame it on the dog.", Name = "Shamefull Gas", Levels = 1, Type = "Curse" });
 
 
 
-
+                //Treasure Cards
                 context.Cards.Add(new LevelUpSpellCard { Description = "Gain 1 Level", FlavorText = "It did it's job.", Name = "Potion of General Usefullness", Level = 1, Type = "Spell" });
                 context.Cards.Add(new LevelUpSpellCard { Description = "Gain 1 Level", FlavorText = "You get a level only because he thinks your mom is hot.", Name = "Whine at the GM", Level = 1, Type = "Spell" });
                 context.Cards.Add(new LevelUpSpellCard { Description = "Gain 1 Level", FlavorText = "Your nerd knowledge is not to be trifled with.", Name = "Invoke Obscure Rules", Level = 1, Type = "Spell" });
@@ -125,8 +130,37 @@ namespace Bunchkins.Migrations
                 context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Body Armor,  Combat Power Bonus: +1", FlavorText = "Slimy and effective.", Slot = "Armor", Gold = 200, Bonus = 1, Name = "Slimy Armor", Type = "Equipment" });
                 context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Headgear,  Combat Power Bonus: +3", FlavorText = "Read the name.", Slot = "Headgear", Gold = 400, Bonus = 3, Name = "Bad-Ass Bandanna", Type = "Equipment" });
                 context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Body Armor,  Combat Power Bonus: +3", FlavorText = "You'll like the way you look. I guarantee it.", Slot = "Armor", Gold = 400, Bonus = 3, Name = "Short Wide Armor", Type = "Equipment" });
-                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hander,  Combat Power Bonus: +4", FlavorText = "Well.....it's pretty.", Slot = "2Hands", Gold = 800, Bonus = 4, Name = "Bow With Ribbons", Type = "Equipment" });
-                
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +4", FlavorText = "Well.....it's pretty.", Slot = "2Hands", Gold = 800, Bonus = 4, Name = "Bow With Ribbons", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +3", FlavorText = "She is just loaning it to you.", Slot = "1Hand", Gold = 400, Bonus = 3, Name = "Broad Sword", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Footgear,  Combat Power Bonus: +2", FlavorText = "Be glad you aren't on the receiving end of these.", Slot = "Footgear", Gold = 400, Bonus = 2, Name = "Boots of Butt-Kicking", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +5", FlavorText = "BURN BABY BURN!!!", Slot = "1Hand", Gold = 800, Bonus = 5, Name = "Staff of Napalm", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Headgear,  Combat Power Bonus: +3", FlavorText = "It wouldn't be the same if it wasn't pointy.", Slot = "Headgear", Gold = 400, Bonus = 3, Name = "Point Hat of Power", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Headgear,  Combat Power Bonus: +1", FlavorText = "The fact that it blocks your vision is probably helping.", Slot = "Headgear", Gold = 200, Bonus = 1, Name = "Helm of Courage", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Headgear,  Combat Power Bonus: +1", FlavorText = "This helmet just thinks of you as a friend....I swear.", Slot = "Headgear", Gold = 300, Bonus = 1, Name = "Horny Helmet", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +1", FlavorText = "The stink is it's only real power.", Slot = "1Hand", Gold = 200, Bonus = 1, Name = "Rat On A Stick", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1Hand,  Combat Power Bonus: +3", FlavorText = "Place gently in a friend's back.", Slot = "1Hand", Gold = 400, Bonus = 3, Name = "Dagger of Treachery", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Body Armor,  Combat Power Bonus: +2", FlavorText = "The burn means it's working.", Slot = "Armor", Gold = 400, Bonus = 2, Name = "Flaming Armor", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +3", FlavorText = "Get all of the guys and gals with this.", Slot = "2Hands", Gold = 300, Bonus = 3, Name = "Tuba of Charm", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Footgear,  Combat Power Bonus: +1", FlavorText = "Run Forest Run.", Slot = "Footgear", Gold = 400, Bonus = 1, Name = "Boots of running REALLY FAST", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +3", FlavorText = "Make your enemies a delicious topping to any taco.", Slot = "1Hand", Gold = 400, Bonus = 3, Name = "Cheese Grater of Peace", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Body Armor,  Combat Power Bonus: +1", FlavorText = "Your hobbies are......interesting.", Slot = "Armor", Gold = 200, Bonus = 1, Name = "Spandex", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Footgear,  Combat Power Bonus: +2", FlavorText = "Your dad bought you this. Oh wait...", Slot = "1Hand", Gold = 400, Bonus = 2, Name = "Sneaky Bastard Sword", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +1", FlavorText = "Now you can touch whatever you want.", Slot = "2Hands", Gold = 200, Bonus = 1, Name = "Eleven-Foot Pole", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Footgear,  Combat Power Bonus: +2", FlavorText = "Basically if crocks didn't suck.", Slot = "Footgear", Gold = 700, Bonus = 2, Name = "Sandals of Protection", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: Body Armor,  Combat Power Bonus: +3", FlavorText = "The best protection for any Hobit on the go.", Slot = "Armor", Gold = 600, Bonus = 3, Name = "Mithril Armor", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +3", FlavorText = "Throw at those who disobey you.", Slot = "2Hands", Gold = 100, Bonus = 3, Name = "Huge Rock", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +4", FlavorText = "For all your adventuring needs, like opening a can of beans.", Slot = "2Hands", Gold = 600, Bonus = 4, Name = "Swiss Army Polearm", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 2 Hands,  Combat Power Bonus: +3", FlavorText = "Great for getting a head in life.", Slot = "2Hands", Gold = 600, Bonus = 3, Name = "Chainsaw of Bloody Dismemberment", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +4", FlavorText = "Always a good thing to be able to defend against GOD.", Slot = "1Hand", Gold = 600, Bonus = 4, Name = "Shield of Ubiquity", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +3", FlavorText = "It's not cheating just because you can hit them but they can't hit you.", Slot = "1Hand", Gold = 600, Bonus = 3, Name = "Rapier of Unfairness", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +2", FlavorText = "For those who wanted to be a pirate but have dyxlesia.", Slot = "1Hand", Gold = 400, Bonus = 2, Name = "Buckler of Swashing", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +4", FlavorText = "Any other mace would have been dull.", Slot = "1Hand", Gold = 600, Bonus = 4, Name = "Mace of Sharpness", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +4", FlavorText = "For todays rapper on the go.", Slot = "1Hand", Gold = 600, Bonus = 4, Name = "Hammer of Kneecapping", Type = "Equipment" });
+                context.Cards.Add(new EquipmentCard { Description = "Equipment Slot: 1 Hand,  Combat Power Bonus: +3", FlavorText = "Pay it a dollar and it may holler.", Slot = "1Hand", Gold = 400, Bonus = 3, Name = "Gentlemen's Club", Type = "Equipment" });
+
+
+
+
                 context.SaveChanges();
                 
             }
